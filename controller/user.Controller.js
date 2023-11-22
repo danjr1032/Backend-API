5dd16ab00185314e15c9f52379af638964dc87b5
danjr1032 <tapjidan@gmail.com>
2023-11-22 20:51:45 +0100

    update tp user controller

--- controller/user.Controller.js
+++ controller/user.Controller.js
@@ -21,7 +21,9 @@ exports.createUser = async (req, res) => {
     });
 
     await newUser.save();
-    res.status(201).json({ message: 'User created successfully', user: newUser });
+    alert("SignUp successfully!");
+    location.href="https://trashpoint.vercel.app/services.html";
+    // res.status(201).json({ message: 'User created successfully', user: newUser });
   } catch (error) {
     res.status(500).json({ message: 'Could not create user', error: error.message });
   }
