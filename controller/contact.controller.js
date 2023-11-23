6535f96b6e66c555c478b1099590bf6ef2fad1ff
danjr1032 <tapjidan@gmail.com>
2023-11-23 19:52:51 +0100

    made changes to contact

--- controller/contact.controller.js
+++ controller/contact.controller.js
@@ -3,7 +3,10 @@ const Message = require ('../models/contact');
 
 
 exports.createMessage = async (req, res) => {
-  const { name, email, message } = req.body;
+  // const { name, email, message } = req.body;
+  const name =req.body.name;
+  const email =req.body.email;
+  const nessage =req.body.message;
 
   try {
     if (name !== "" && email !== "" && message !== "") {
