92531ef86441408cb3aa97faa073fe0bc4b5821e
danjr1032 <tapjidan@gmail.com>
2023-11-23 14:07:38 +0100

    new update 3

--- controller/contact.controller.js
+++ controller/contact.controller.js
@@ -5,40 +5,17 @@ exports.createMessage = async (req, res) => {
   const { name, email, message } = req.body;
 
   try {
-    if (!name || !email || !message) {
-      return res.status(400).json({ success: false, error: 'All fields are required' });
-    }
-    else {
-      const newMessage = new Message({ name, email, message });
-  
-      await newMessage.save();
-      res.status(201).json({ success: true, message: 'Message saved successfully' });
-      res.redirect("https://trashpoint.vercel.app/index.html");
-    }
+    const newMessage = new Message({ name, email, message });
 
-    
+    await newMessage.save();
+    res.redirect("https://trashpoint.vercel.app/index.html");
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: 'Error saving message' });
   }
 };
 
-// exports.createMessage = async (req, res) => {
-//     const { name, email, message } = req.body;
-    
-//     try {
-//       const sendMessage = new Message({ name, email, message });
-//       await sendMessage.save();
-  
-//       res.redirect("https://trashpoint.vercel.app/index.html");c
 
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ success: false, error: 'Error sending message' });
-//     }
-//   };
-  
-  
 
 exports.getAllMessages = async (req, res) => {
   try {
