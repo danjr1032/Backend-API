58000f45f4960e03651a4e2fe67afd761631ede5
danjr1032 <tapjidan@gmail.com>
2023-11-23 12:04:52 +0100

    update

--- controller/contact.controller.js
+++ controller/contact.controller.js
@@ -3,30 +3,19 @@
 const Message = require('../models/contact');
 
 exports.createMessage = async (req, res) => {
-  const {name, email, message } = req.body
+    const { name, email, message } = req.body;
   
-  try {
-    const newMessage = new Message({ 
-      name:name, 
-      email:email, 
-      message:message
-    });
-    await newMessage.save();
-
-    res.redirect("https://trashpoint.vercel.app/#");
-    res.status(201).json({message: "Message sent successfully!"});
-
-    // return { 
-    //   // res.status(200),
-    //   success: true, 
-    //   message: 'Message sent successfully', 
-    //   data: newMessage };
-  } catch (error) {
-    console.error(error);
-    res.json(400)
-    return { success: false, error: 'Error sending message' };
-  }
-};
+    try {
+      const newMessage = new Message({ name, email, message });
+      await newMessage.save();
+  
+      res.redirect("https://trashpoint.vercel.app/#");
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, error: 'Error sending message' });
+    }
+  };
+  
 
 
 exports.getAllMessages = async (req, res) => {
