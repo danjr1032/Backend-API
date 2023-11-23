b7108f8d19072b7e886a39ded4a28d5a310a65c7
danjr1032 <tapjidan@gmail.com>
2023-11-23 16:42:20 +0100

    update

--- controller/contact.controller.js
+++ controller/contact.controller.js
@@ -1,32 +1,72 @@
 
-const Message = require('../models/contact');
+const Message = require ('../models/contact');
 
-exports.createMessage = async (req, res) => {
-  const { name, email, message } = req.body;
+  exports.createMessage = async (req, res) => {
+    try {
+      const { name, email, message } = req.body;
 
-  try {
-    if (name!=="" && email!=="" && message!=="") {
-      const newMessage = new Message({ name, email, message });
-  
+      if (!name || !email || !message) {
+        return res.status(400).json({ error: 'Please provide name, email, and message.' });
+      }
+      const newMessage = new Message({
+        name,
+        email,
+        message,
+      });
+
       await newMessage.save();
       res.redirect("https://trashpoint.vercel.app/index.html");
+      // res.status(201).json({ message: 'Message created successfully.', data: newMessage });
+    } catch (error) {
+      console.error('Error creating message:', error);
+      res.status(500).json({ error: 'Internal server error' });
     }
-  } 
-  catch (error) {
-    console.error(error);
-    res.json({error: 'Error saving message' });
-  }
-};
+  };
+
+  
+  exports.getAllMessages = async (req, res) => {
+    try {
+      const messages = await Message.find();
+      res.status(200).json({ data: messages });
+    } 
+    catch (error) {
+      console.error('Error fetching messages:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
 
 
 
-exports.getAllMessages = async (req, res) => {
-  try {
-    const messages = await Message.find();
-    res.json({ success: true, data: messages });
-  } catch (error) {
-    res.json({ success: false, error});
-  }
-};
+
+// const Message = require('../models/contact');
+
+// exports.createMessage = async (req, res) => {
+//   const { name, email, message } = req.body;
+
+//   try {
+//     if (name!=="" && email!=="" && message!=="") {
+
+//       const newMessage = new Message({ name, email, message });
+  
+//       await newMessage.save();
+//       res.redirect("https://trashpoint.vercel.app/index.html");
+//     }
+//   } 
+//   catch (error) {
+//     console.error(error);
+//     res.json({error: 'Error saving message' });
+//   }
+// };
+
+
+
+// exports.getAllMessages = async (req, res) => {
+//   try {
+//     const messages = await Message.find();
+//     res.json({ success: true, data: messages });
+//   } catch (error) {
+//     res.json({ success: false, error});
+//   }
+// };
 
  
--- models/contact.js
+++ models/contact.js
@@ -4,20 +4,22 @@ const schema = mongoose.Schema;
 const messageSchema = new schema({
    name: {
     type: String,
+    required: true,
   },
 
   email: {
     type: String,
+    required: true,
   },
 
   message: {
     type: String,
+    required: true,
   }
 
 });
 
 const Message = mongoose.model('Message', messageSchema);
-
 module.exports = Message;
 
 
