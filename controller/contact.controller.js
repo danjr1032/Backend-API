7f684faf5394d00fc9d159589a06119e69e65995
danjr1032 <tapjidan@gmail.com>
2023-11-23 14:16:37 +0100

    new update 4

--- controller/contact.controller.js
+++ controller/contact.controller.js
@@ -5,13 +5,16 @@ exports.createMessage = async (req, res) => {
   const { name, email, message } = req.body;
 
   try {
-    const newMessage = new Message({ name, email, message });
-
-    await newMessage.save();
-    res.redirect("https://trashpoint.vercel.app/index.html");
-  } catch (error) {
+    if (name!=="" && email!=="" && message!=="") {
+      const newMessage = new Message({ name, email, message });
+  
+      await newMessage.save();
+      res.redirect("https://trashpoint.vercel.app/index.html");
+    }
+  } 
+  catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, error: 'Error saving message' });
+    res.json({error: 'Error saving message' });
   }
 };
 
@@ -22,7 +25,7 @@ exports.getAllMessages = async (req, res) => {
     const messages = await Message.find();
     res.json({ success: true, data: messages });
   } catch (error) {
-    res.status(500).json({ success: false, error});
+    res.json({ success: false, error});
   }
 };
 
