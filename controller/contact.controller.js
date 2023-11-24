b6afc326e84e11ae4cf35bf77035e307459fe6c4
danjr1032 <tapjidan@gmail.com>
2023-11-24 08:19:38 +0100

    update to contact

--- controller/contact.controller.js
+++ controller/contact.controller.js
@@ -1,8 +1,6 @@
-const express = require('express');
 const Message = require('../models/contact');
 
 
-// router.get('/messages', 
 exports.messages = async (req, res) => {
   try {
     const messages = await Message.find();
@@ -12,16 +10,19 @@ exports.messages = async (req, res) => {
   }
 };
 
-// router.post('/send',
 exports.sendMessage = async (req, res) => {
   const { name, email, message } = req.body;
 
   try {
+
     const newMessage = new Message({ name, email, message });
     const savedMessage = await newMessage.save();
     res.status(201).json(savedMessage);
+
   } catch (error) {
+
     res.status(400).json({ error: 'Bad Request' });
+
   }
 };
 
--- models/contact.js
+++ models/contact.js
@@ -2,22 +2,22 @@ const mongoose = require ("mongoose");
 const schema = mongoose.Schema;
 
 const messageSchema = new schema({
-   name: {
-    type: String,
+  name: {
+  type: String,
+   required: true
   },
-
   email: {
     type: String,
-
+    required: true
   },
   message: {
     type: String,
-
-  }
-
+    required: true
+  },
+  
 });
 
+
 const Message = mongoose.model('Message', messageSchema);
+
 module.exports = Message;
-
-
