30743501a9bfcb29d0c0abfac054a9a30e53051c
danjr1032 <tapjidan@gmail.com>
2023-12-14 14:43:19 +0100

    update

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ const store = new MongoDBStore({
   store.on('error', function(error) {
     console.log(error);
   });
-
+  
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -47,12 +48,6 @@ app.use(
   );
 
 
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
 
 app.use('/user', userRoute);
 app.use('/request',  requestRouter);
